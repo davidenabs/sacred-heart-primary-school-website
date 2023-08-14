bf7b12506dbe73a35806ea943fdb1954b9eeba53
David Enabs <davidenabs@gmail.com>
2023-08-14 15:03:36 +0100

    Added google tracking to the guest/app.blade file

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -2,6 +2,18 @@
 <html lang="en">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XNZCH0D873"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-XNZCH0D873');
+    </script>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
